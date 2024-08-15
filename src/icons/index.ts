cede9867b6550c9c57a3a9ac3e2ea4c2c1b09de7
Renato <renatoalves92@gmail.com>
2024-08-15 20:44:20 -0300

    feat: create svg icons

---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as HomeIcon } from './Home.icon'
+export { default as SearchIcon } from './Search.icon'
+export { default as MoonIcon } from './Moon.icon'
+export { default as SunIcon } from './Sun.icon'
