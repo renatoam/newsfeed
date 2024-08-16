f81320f1a97da141331f5f52650d4d7d19dc9bc1
Renato <renatoalves92@gmail.com>
2024-08-15 21:36:33 -0300

    chore: create barrel exports for card fragments

---
+++ src/components/Card/fragments/index.ts
@@ -0,0 +1,8 @@
+export { default as CardActionsGroup } from "./CardActionsGroup";
+export { default as CardBody } from "./CardBody";
+export { default as CardCategories } from "./CardCategories";
+export { default as CardCloseButton } from "./CardCloseButton";
+export { default as CardDescription } from "./CardDescription";
+export { default as CardImage } from "./CardImage";
+export { default as CardTitle } from "./CardTitle";
+export { default as CardWrapper } from "./CardWrapper";
